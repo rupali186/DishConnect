3bc744dc36e6a20132398cace41a10c5f7b4fddb
rupali186 <35391407+rupali186@users.noreply.github.com>
2018-06-23 12:56:39 +0530

    nav UI set

--- app/src/main/java/com/example/rupali/dishconnect/MainActivity.java
+++ app/src/main/java/com/example/rupali/dishconnect/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_buy) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_dish_story) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_reviews) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_about) {
 
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_source_code) {
 
         }
 
